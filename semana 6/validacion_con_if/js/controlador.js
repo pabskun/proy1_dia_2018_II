e7c7d6fac36f381e42c72a495e666867b5a7440e
pabskun <pablo.monestel@gmail.com>
2018-06-15 09:58:07 -0600

    Commit
    Refactor

---
+++ semana 6/validacion_con_if/js/controlador.js
@@ -0,0 +1,127 @@
+'use strict';
+
+let botonRegistrar = document.querySelector('#btnRegistrar');
+botonRegistrar.addEventListener('click', obtenerDatos);
+
+let inputNacimiento = document.querySelector('#txtFecha');
+inputNacimiento.addEventListener('change', calcularEdad);
+
+let inputNombre = document.querySelector('#txtNombre');
+let inputEdad = document.querySelector('#txtEdad');
+let inputContrasenna = document.querySelector('#txtContrasenna');
+let inputConfirmacion = document.querySelector('#txtConfirmacion');
+
+let sNombre = '';
+let dFechaNacimiento = '';
+let nEdad = 0;
+let sContrasenna = '';
+let sConfirmacion = '';
+
+let regexSoloLetras = /^[a-zA-Z]+$/;
+let regexSoloNumeros = /^[0-9]+$/;
+let regexContrasenna = /^[a-zA-Z0-9]{8,12}$/;
+
+function obtenerDatos(){
+    let bError = false;
+
+    sNombre = inputNombre.value;
+    dFechaNacimiento = inputNacimiento.value;
+    nEdad = inputEdad.value;
+    sContrasenna = inputContrasenna.value;
+    sConfirmacion = inputConfirmacion.value;
+
+    bError = validar();
+    
+    if(bError == true){
+        swal({
+            title: 'Registro incorrecto',
+            text: 'No se pudo registrar el usuario, revise los campos en rojo',
+            type: 'warning',
+            confirmButtonText: 'Entendido'
+          });
+    }else{
+        swal({
+            title: 'Registro correcto',
+            text: 'El usuario se ha registrado de forma correcta',
+            type: 'success',
+            confirmButtonText: 'Entendido'
+          });
+        console.log(sNombre);
+        console.log(dFechaNacimiento);
+        console.log(nEdad);
+        console.log(sContrasenna);
+        console.log(sConfirmacion);
+        limpiarFormulario();
+    }
+    
+};
+
+function validar(){
+    let bError = false;
+
+    sNombre = inputNombre.value;
+    dFechaNacimiento = new Date(inputNacimiento.value);
+    nEdad = Number(inputEdad.value);
+    sContrasenna = inputContrasenna.value;
+    sConfirmacion = inputConfirmacion.value;
+
+    // Validación del nombre
+    if(sNombre == '' || (regexSoloLetras.test(sNombre) == false) ){
+        inputNombre.classList.add('errorInput');
+        bError = true;
+    }else{
+        inputNombre.classList.remove('errorInput');
+    }
+    // Validación de la fecha de nacimiento
+    if(dFechaNacimiento == '' || dFechaNacimiento > Date() ) {
+        inputNacimiento.classList.add('errorInput');
+        bError = true;
+    }else{
+        inputNacimiento.classList.remove('errorInput');
+    }
+    // Validación de la edad
+    if(nEdad == 0 || (regexSoloNumeros.test(nEdad) == false) || nEdad < Number(inputEdad.min) || nEdad > Number(inputEdad.max)){
+        inputEdad.classList.add('errorInput');
+        bError = true;
+    }else{
+        inputEdad.classList.remove('errorInput');
+    }
+
+   
+    // Validación de la contraseña y confirmación
+    if(sContrasenna != sConfirmacion){
+        inputContrasenna.classList.add('errorInput');
+        inputConfirmacion.classList.add('errorInput');
+        bError = true;
+    }else{
+        inputContrasenna.classList.remove('errorInput');
+        inputConfirmacion.classList.remove('errorInput');
+    }
+
+     // Validación de la contraseña
+     if(sContrasenna == '' || regexContrasenna.test(sContrasenna) == false){
+        inputContrasenna.classList.add('errorInput');
+        bError = true;
+    }else{
+        inputContrasenna.classList.remove('errorInput');
+    }
+
+    return bError;
+};
+
+function calcularEdad(){
+    let inputNacimiento = document.querySelector('#txtFecha');
+    let dFechaNacimiento = new Date(inputNacimiento.value); 
+
+    let nEdad = new Date().getFullYear() - dFechaNacimiento.getFullYear();
+
+    document.querySelector('#txtEdad').value = nEdad;
+}
+
+function limpiarFormulario(){
+    inputNombre.value = '';
+    inputEdad.value = 0;
+    inputNacimiento.value = '';
+    inputContrasenna.value = '';
+    inputConfirmacion.value = '';
+};
---
+++ semana 6/validacion_con_if/js/servicio.js
@@ -0,0 +1,11 @@
+'use strict';
+let listaPersonas = [];
+
+function registrarPersona(paInfoPersona){
+    listaPersonas.push(paInfoPersona);
+};
+
+function obtenerListaPersonas(){
+    return listaPersonas;
+};
+
